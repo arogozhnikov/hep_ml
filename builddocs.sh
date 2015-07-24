50cfb8e22da8b40ea4716f41dd0af72f4a33a0e5
Alexey <iamfullofspam@gmail.com>
2015-07-24 17:11:10 +0300

    added script to build docs

---
+++ builddocs.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# assuming that in ../hepml_doc there is another clone of repository
+
+
+cd docs
+# Forcing to ignore caches
+make SPHINXOPTS="-E" html
+cd ..
+# TODO delete there evrything for the except of git folder
+# currently we simply copy, while leaving everythong there
+rsync -avh docs/_build/html/* ../hep_mldoc
+
+cd ../hep_mldoc
+touch .nojekyll
+
+git add .
+git commit -am 'autoupdate of documentation'
+git push origin gh-pages
+
+# return back
+cd ../hep_ml
