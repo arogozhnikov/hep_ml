8912cf3cb750862f8dab35d54b0440626eb6cdfc
Alexey <iamfullofspam@gmail.com>
2015-07-27 14:23:41 +0300

    added GB regressor,  renaming

--- hep_ml/gradientboosting.py
+++ hep_ml/gradientboosting.py
@@ -18,8 +18,7 @@ from __future__ import print_function, division, absolute_import
 import copy
 import numpy
 
-from sklearn.base import BaseEstimator, ClassifierMixin
-from sklearn.tree.tree import DecisionTreeRegressor, DTYPE
+from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 from sklearn.utils.random import check_random_state
 
 from .commonutils import score_to_proba, check_xyw
@@ -30,9 +29,12 @@ from .losses import AbstractLossFunction, AdaLossFunction, \
 
 
 __author__ = 'Alex Rogozhnikov'
+__all__ = ['UGradientBoostingClassifier', 'UGradientBoostingRegressor' ]
 
 
-class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
+class UGradientBoostingBase(BaseEstimator):
+    """ Base class for gradient boosting estimators """
+
     def __init__(self, loss=None,
                  n_estimators=100,
                  learning_rate=0.1,
@@ -42,15 +44,11 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
                  max_features=None,
                  max_leaf_nodes=None,
                  max_depth=3,
-                 criterion='mse',
                  splitter='best',
                  update_tree=True,
                  train_features=None,
                  random_state=None):
         """
-        This version of gradient boosting supports only two-class classification and only special losses
-        derived from AbstractLossFunction.
-
         `max_depth`, `max_leaf_nodes`, `min_samples_leaf`, `min_samples_split`, `max_features` are parameters
         of regression tree, which is used as base estimator.
 
@@ -76,7 +74,6 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         self.update_tree = update_tree
         self.train_features = train_features
         self.random_state = random_state
-        self.criterion = criterion
         self.splitter = splitter
         self.classes_ = [0, 1]
 
@@ -93,15 +90,24 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         leaves = tree.transform(X)
         return leaf_values[leaves]
 
-    def fit(self, X, y, sample_weight=None):
-        """
-        Only two-class binary classification is supported with labels 0 and 1.
+    # def _find_best_constant(self, loss, n_samples, learning_rate=0.5, n_iterations=4):
+    #     """Find best initial constant (in case the loss function doesn't do it automatically)
+    #
+    #     :param loss: fitted loss function
+    #     :param n_iterations: number of steps in iterative procedure
+    #     :return: float, best initial constant.
+    #     """
+    #     y_pred = numpy.zeros(n_samples, dtype=float)
+    #     result = 0.
+    #     terminal_regions = numpy.zeros(n_samples, dtype=int)
+    #     for _ in range(n_iterations):
+    #         residual, weights = self.loss.prepare_tree_params(y_pred + result)
+    #         AdaLossFunction.prepare_new_leaves_values(terminal_regions, leaf_values=numpy.array([numpy.mean(loss)]),
+    #                                                   )
+    #         self.loss.prepare_new_leaves_values()
 
-        :param X: dataset of shape [n_samples, n_features]
-        :param y: labels, array-like of shape [n_samples]
-        :param sample_weight: array-like of shape [n_samples] or None
-        :return: self
-        """
+
+    def fit(self, X, y, sample_weight=None):
         self._check_params()
 
         self.estimators = []
@@ -109,17 +115,17 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
 
         n_samples = len(X)
         n_inbag = int(self.subsample * len(X))
+        update_mask = numpy.ones(len(X), dtype=bool)
 
         # preparing for loss function
-        X, y, sample_weight = check_xyw(X, y, sample_weight=sample_weight, classification=True)
-        sample_weight = sample_weight * 1. / numpy.mean(sample_weight)
+        X, y, sample_weight = check_xyw(X, y, sample_weight=sample_weight)
 
         self.loss = copy.deepcopy(self.loss)
         self.loss.fit(X, y, sample_weight=sample_weight)
 
         # preparing for fitting in trees, setting appropriate DTYPE
         X = self._get_train_features(X)
-        X = numpy.array(X, dtype=DTYPE)
+        X = SklearnClusteringTree.prepare_data(X)
         self.n_features = X.shape[1]
 
         y_pred = numpy.zeros(len(X), dtype=float)
@@ -127,7 +133,7 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         for stage in range(self.n_estimators):
             # tree creation
             tree = SklearnClusteringTree(
-                criterion=self.criterion,
+                criterion='mse',
                 splitter=self.splitter,
                 max_depth=self.max_depth,
                 min_samples_split=self.min_samples_split,
@@ -147,8 +153,9 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
             if self.update_tree:
                 terminal_regions = tree.transform(X)
                 leaf_values = self.loss.prepare_new_leaves_values(terminal_regions, X=X, y=y, y_pred=y_pred,
-                    leaf_values=leaf_values, sample_weight=sample_weight,
-                    update_mask=numpy.ones(len(X), dtype=bool), residual=residual)
+                                                                  leaf_values=leaf_values, sample_weight=sample_weight,
+                                                                  update_mask=update_mask,
+                                                                  residual=residual)
 
             y_pred += self.learning_rate * self._estimate_tree(tree, leaf_values=leaf_values, X=X)
             self.estimators.append([tree, leaf_values])
@@ -167,7 +174,7 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         :param X: data
         :return: sequence of numpy.array of shape [n_samples]
         """
-        X = numpy.array(self._get_train_features(X), dtype=DTYPE)
+        X = SklearnClusteringTree.prepare_data(self._get_train_features(X))
         y_pred = numpy.zeros(len(X))
         for tree, leaf_values in self.estimators:
             y_pred += self.learning_rate * self._estimate_tree(tree, leaf_values=leaf_values, X=X)
@@ -183,6 +190,36 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         for score in self.staged_decision_function(X):
             result = score
         return result
+
+    @property
+    def feature_importances_(self):
+        """Returns feature importances for all features used in training.
+        The order corresponds to the order in `self.train_features`
+
+        :return: numpy.array of shape [n_train_features]
+        """
+        import warnings
+
+        warnings.warn('feature_importances_ of gb returns importances corresponding to used columns ')
+        total_sum = sum(tree.feature_importances_ for tree, values in self.estimators)
+        return total_sum / len(self.estimators)
+
+
+class UGradientBoostingClassifier(UGradientBoostingBase, ClassifierMixin):
+    """This version of gradient boosting supports only two-class classification and only special losses
+    derived from AbstractLossFunction."""
+
+    def fit(self, X, y, sample_weight=None):
+        """Train formula.
+        Only two-class binary classification is supported with labels 0 and 1.
+
+        :param X: dataset of shape [n_samples, n_features]
+        :param y: labels, array-like of shape [n_samples]
+        :param sample_weight: array-like of shape [n_samples] or None
+        :return: self
+        """
+        X, y, sample_weight = check_xyw(X, y, sample_weight=sample_weight, classification=True)
+        return UGradientBoostingBase.fit(self, X, y, sample_weight=sample_weight)
 
     def staged_predict_proba(self, X):
         """Predicted probabilities for each event
@@ -209,14 +246,34 @@ class GradientBoostingClassifier(BaseEstimator, ClassifierMixin):
         """
         return numpy.argmax(self.predict_proba(X), axis=1)
 
-    @property
-    def feature_importances_(self):
-        """Returns feature importances for all features used in training.
-        The order corresponds to the order in `self.train_features`
 
-        :return: numpy.array of shape [n_train_features]
+class UGradientBoostingRegressor(UGradientBoostingBase, RegressorMixin):
+    """This version of gradient boosting supports only two-class classification and only special losses
+    derived from AbstractLossFunction."""
+
+    def fit(self, X, y, sample_weight=None):
+        """Fit estimator.
+
+        :param X: dataset of shape [n_samples, n_features]
+        :param y: target values, array-like of shape [n_samples]
+        :param sample_weight: array-like of shape [n_samples] or None
+        :return: self
         """
-        import warnings
-        warnings.warn('feature_importances_ of gb returns importances corresponding to used columns ')
-        total_sum = sum(tree.feature_importances_ for tree, values in self.estimators)
-        return total_sum / len(self.estimators)
+        return UGradientBoostingBase.fit(self, X, y, sample_weight=sample_weight)
+
+    def staged_predict(self, X):
+        """Return predictions after each new tree
+
+        :param X: data
+        :return: sequence of numpy.array of shape [n_samples]
+        """
+        for score in self.staged_decision_function(X):
+            yield score
+
+    def predict(self, X):
+        """Predict values for new samples
+
+        :param X: pandas.DataFrame with all train_features
+        :return: numpy.array of shape [n_samples]
+        """
+        return self.decision_function(X)
