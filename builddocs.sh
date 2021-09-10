1b65b820a80eff425490f4691959826d4bf95bd1
Alex Rogozhnikov <iamfullofspam@gmail.com>
2021-09-10 11:50:03 -0700

    docs v0.7

--- builddocs.sh
+++ builddocs.sh
@@ -1,31 +1,12 @@
 #!/usr/bin/env bash
-# script should be running from its directory
-# assumed that in ../hepml_doc there is another clone of repository
-# with gh-pages branch active
+# script should be sourced from repo root
+# documentation is served from docs/ folder, not separate branch
 
-
-cd docs
+pip install sphinx_rtd_theme
+pip install -e .
+pushd docs
 # Forcing to ignore caches
 make SPHINXOPTS="-E" html
-cd ..
-
-# deleting everythong for the exception
-# of .git/ and .gitignore
-mv ../hep_mldoc/.git/ /tmp/githepmldoc
-mv ../hep_mldoc/.gitignore /tmp/gitignorehepmldoc
-rm -r ../hep_mldoc/*
-mv /tmp/githepmldoc ../hep_mldoc/.git/
-mv /tmp/gitignorehepmldoc ../hep_mldoc/.gitignore
-
-# copying new files to hep_mldoc
-rsync -avh docs/_build/html/* ../hep_mldoc
-
-cd ../hep_mldoc
 touch .nojekyll
+popd
 
-git add .
-git commit -am 'autoupdate of documentation'
-git push origin gh-pages
-
-# return
-cd ../hep_ml
