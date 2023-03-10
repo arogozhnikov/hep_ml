4e91af53c1d85dcc0060bc9ea4da119799ff2689
Alex Rogozhnikov <iamfullofspam@gmail.com>
2023-03-10 10:30:35 -0800

    delete folders within distributions

--- publish_to_pypi.sh
+++ publish_to_pypi.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # simple script to upload new versions to PyPI
 
-rm -f build/*
-rm -f dist/*
+rm -rf build/*
+rm -rf dist/*
 python3 -m pip install --upgrade build twine
 python3 -m build
 python3 -m twine upload dist/*
