e87bccdbff65944d094b27cf157f60470771b238
Jonas Eschle <mayou36@jonas.eschle.com>
2025-06-17 14:09:05 +0200

    docs: build docs

--- builddocs.sh
+++ builddocs.sh
@@ -2,8 +2,7 @@
 # script should be sourced from repo root
 # documentation is served from docs/ folder, not separate branch
 
-pip install sphinx_rtd_theme
-pip install -e .
+pip install -e .[docs]
 pushd docsrc
 # Forcing to ignore caches
 make SPHINXOPTS="-E" html
